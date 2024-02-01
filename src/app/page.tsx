72e62ddd5dfef1d9fb1bab3850c53d9d63cf7a51
Jeffrey <matheusunitt@gmail.com>
2024-02-01 10:38:59 -0300

    Adiciona componente Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Nav from "@/components/Nav";
+import Banner from "@/components/Banner";
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <div className="container my-8 px-2 sm:px-8">
+        <Banner />
+      </div>
     </>
   );
 };
---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,7 @@
+const Banner = () => {
+  return (
+    <div className="h-[250px] w-full p-8 rounded-lg bg-[#CECECE]">Banner</div>
+  );
+};
+
+export default Banner;
