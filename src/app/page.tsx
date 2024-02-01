1b20f1c5ff9d0bcfac33e035d0c87e7ad90ad207
Jeffrey <matheusunitt@gmail.com>
2024-02-01 10:47:48 -0300

    Adiciona componente ItemsList na homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Nav from "@/components/Nav";
 import Banner from "@/components/Banner";
+import ItemsList from "@/components/ItemsList";
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <div className="container my-8 px-2 sm:px-8">
+      <div className="flex flex-col container my-8 px-2 gap-6 sm:px-8">
         <Banner />
+        <ItemsList />
       </div>
     </>
   );
---
+++ src/components/ItemsList/index.tsx
@@ -0,0 +1,7 @@
+const ItemsList = () => {
+  return (
+    <div className="h-max w-full p-8 rounded-lg bg-[#F3F3F3]">ItemsList</div>
+  );
+};
+
+export default ItemsList;
