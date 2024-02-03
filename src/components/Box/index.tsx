906424912336cb6df0a32e6c57b28d6fad610857
Jeffrey <matheusunitt@gmail.com>
2024-02-03 10:06:18 -0300

    Adiciona flex-col no Box

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,6 +1,6 @@
 const Box = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-max w-full p-10 rounded-lg bg-[#F3F3F3] sm:p-20">
+    <div className="flex flex-col gap-10 h-max w-full p-10 rounded-lg bg-[#F3F3F3] sm:p-20">
       {children}
     </div>
   );
