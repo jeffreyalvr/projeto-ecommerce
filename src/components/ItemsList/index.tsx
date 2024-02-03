5a48430668c9a59d4ad6290082b49f061b058247
Jeffrey <matheusunitt@gmail.com>
2024-02-03 10:50:05 -0300

    Adiciona valor e edita o design dos itens da lista

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 import no_image_image from "/public/icons/no-image.png";
 import like_outlined_image from "/public/icons/like-outlined.png";
 import like_filled_image from "/public/icons/like-filled.png";
+import add_to_cart_image from "/public/icons/add-to-cart.png";
 
 const ItemsList = async () => {
+  type Item = {
+    id: string;
+    nome: string;
+    valor: number;
+    estoque: number;
+    descricao: string;
+    fotoUrl: string;
+  };
+
   const getItens = async () => {
     const response = await fetch(
       "https://projeto-ecommerce-api.onrender.com/produtos",
@@ -17,7 +27,7 @@ const ItemsList = async () => {
 
   return (
     <div className="grid grid-cols-4 gap-8 place-items-center h-max w-full p-8 rounded-lg bg-[#F3F3F3]">
-      {itens.map((item) => (
+      {itens.map((item: Item) => (
         <div className="flex flex-col gap-2 w-[200px] h-auto">
           <div className="inline-flex w-full h-[200px] rounded-lg bg-[#CECECE]">
             <Image
@@ -26,13 +36,21 @@ const ItemsList = async () => {
               className="size-fit m-auto"
             />
           </div>
-          <div className="flex items-center justify-between">
-            <span>{item.nome}</span>
-            <Image
-              src={like_outlined_image}
-              alt="Favoritar"
-              className="size-[32px]"
-            />
+          <div className="flex flex-col">
+            <p className="font-bold">{item.nome}</p>
+            <span className="text-[#6B6B6B]">R$ {item.valor}</span>
+            <div className="flex gap-1 w-max mt-2">
+              <Image
+                src={like_outlined_image}
+                alt="Favoritar"
+                className="size-[32px] cursor-pointer"
+              />
+              <Image
+                src={add_to_cart_image}
+                alt="Favoritar"
+                className="size-[32px] cursor-pointer"
+              />
+            </div>
           </div>
         </div>
       ))}
